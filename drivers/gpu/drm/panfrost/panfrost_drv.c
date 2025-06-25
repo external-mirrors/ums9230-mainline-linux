7cd2fae69ac15a84b5dd62ba18031524c4dfc185
Otto Pflüger <otto.pflueger@abscue.de>
2025-06-25 12:57:39 +0200

    drm: panfrost: enable runtime clock gating on UMS9230
    Add an entry for the sprd,ums9230-mali compatible to enable runtime
    power management.
    
    Signed-off-by: Otto Pflüger <otto.pflueger@abscue.de>

--- drivers/gpu/drm/panfrost/panfrost_drv.c
+++ drivers/gpu/drm/panfrost/panfrost_drv.c
@@ -1178,6 +1178,7 @@ static const struct of_device_id dt_match[] = {
 	{ .compatible = "mediatek,mt8192-mali", .data = &mediatek_mt8192_data },
 	{ .compatible = "mediatek,mt8370-mali", .data = &mediatek_mt8370_data },
 	{ .compatible = "allwinner,sun50i-h616-mali", .data = &default_pm_rt_data },
+	{ .compatible = "sprd,ums9230-mali", .data = &default_pm_rt_data },
 	{}
 };
 MODULE_DEVICE_TABLE(of, dt_match);
